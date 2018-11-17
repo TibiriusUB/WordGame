a7225adcdf95849345ff92f271f26869229b276c
Tibirius <tibirius@gmail.com>
2018-11-17 00:53:13 -0500

    take 30

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
-var gamesSelection = ["pa c-man","ce nt-ipede","ga lax-ian"]
+// set all variables
+var gamesSelection = ["pac-man","centipede","galaxian","asteroids"]
 var wins = 0
 var loss = 0
 var leTTers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 document.querySelector("#wins").innerHTML ="Wins: "+ wins
 document.querySelector("#losses").innerHTML ="Losses: "+ loss
-//  Game starts here, it will be one big loop. 
-document.onkeyup = function() { 
+game()
+document.onkeyup = function(game) { 
     
     var gamePick = Math.floor(Math.random()*gamesSelection.length);
     var plays = 9;
@@ -18,21 +19,30 @@ document.onkeyup = function() {
                  else if (target[i] === " ") {tarDisp.push(" ")}
                         else {tarDisp.push("_")}
     }
-    document.querySelector("#display").innerHTML = tarDisp ;
+    document.querySelector("#display").innerHTML = tarDisp.join(" ") ;
+// starting the actuall game
 
     console.log (target.length)
     console.log (target)
     console.log (plays)
     console.log (tarDisp)
- //starting the actuall game
-for ( i =9;  i > 0; i--)  {
-    document.querySelector("#lives").innerHTML = "lives: " + plays ;
-      
-
+}
 // the score will display itself after each sub loop 
-}
-}
+        // for ( j =plays;  j > 0; j--)  {
+        //     document.querySelector("#lives").innerHTML = "lives: " + plays;
+  //          document.onkeyup function(lettercheck){
+        //     for (l=0;l<target.length;l++) {
+        //         if (target[l] === x) {console.log("check")}
+        //     }
+         
+        // }
+    //}
 
+  //  }
+
+//   document.querySelector("#wins").innerHTML ="Wins: "+ wins
+//   document.querySelector("#losses").innerHTML ="Losses: "+ loss
+//   document.onkeyup = function() { 
 //   THIS SHOULD COME IN HANDY!
 // .forEach
 // var userInput = event.key.toLowerCase();
